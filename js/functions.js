d4fbd6711200325ea765f640189c5725ac91577c
Anka Kozłowska <kozlowskanka@gmail.com>
2019-10-08 22:00:49 +0200

    Practice let and const

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,2 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
 
-function computerResult (){
-	let computerScore = document.getElementById('computerResult');
-	computerScore.innerHTML = ++computerScore.innerHTML;
-
-}
-
-function playerResult (){
-	let playerScore = document.getElementById('playerResult');
-	playerScore.innerHTML = ++playerScore.innerHTML;
-}
--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
 function playGame (playerInput) {
-    
-  clearMessages ();
+  
+  const clearMessages = function (){
+    document.getElementById('messages').innerHTML = '';
+  }
+
+  clearMessages();
+
+  const printMessage = function(msg){
+    var div = document.createElement('div');
+    div.innerHTML = msg;
+    document.getElementById('messages').appendChild(div);
+  }
 
   let randomNumber = Math.floor(Math.random() * 3 + 1);
-
   console.log('Wylosowana liczba to: ' + randomNumber);
+
+  let computerMove = getMoveName(randomNumber);
+  console.log('Komputer pokazał: ' + computerMove);
+
+  let playerMove = getMoveName(playerInput);
+  console.log('Gracz pokazał: ' + playerMove);
 
   function getMoveName(MoveId){
     if(MoveId == 1){
@@ -19,15 +34,15 @@ function playGame (playerInput) {
     }
   }
 
-  let computerMove = getMoveName(randomNumber);
-
-  console.log('Komputer pokazał: ' + computerMove);
-
-  let playerMove = getMoveName(playerInput);
-
-  console.log('Gracz pokazał: ' + playerMove);
-
-  function displayResult(computerMove, playerMove){
+  const displayResult = function(computerMove, playerMove){
+    const computerResult = function (){
+      let computerScore = document.getElementById('computerResult');
+      computerScore.innerHTML = ++computerScore.innerHTML;
+    }
+    const playerResult = function(){
+      let playerScore = document.getElementById('playerResult');
+      playerScore.innerHTML = ++playerScore.innerHTML;
+    }
     
     console.log('Wywołanie funkcji rozstrzygnięcia');	
     console.log('moves:', computerMove, playerMove);
@@ -60,15 +75,18 @@ function playGame (playerInput) {
 
 }
 
-document.getElementById('play-rock').addEventListener('click', function(){
-  playGame(1);
-});
+const playerClick = function() {
+  document.getElementById('play-rock').addEventListener('click', function(){
+    playGame(1);
+  });
 
-document.getElementById('play-paper').addEventListener('click', function(){
-  playGame(2);
-});
+  document.getElementById('play-paper').addEventListener('click', function(){
+    playGame(2);
+  });
 
-document.getElementById('play-scissors').addEventListener('click', function(){
-  playGame(3);
-});
+  document.getElementById('play-scissors').addEventListener('click', function(){
+    playGame(3);
+  });
+}
 
+playerClick();
